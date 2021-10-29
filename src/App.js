c69e1446fbd626399416397abff92f362ee3dbe6
Subham Chowdhury <66264384+SubhamChowdhury@users.noreply.github.com>
2021-10-29 23:10:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import Users from "./Components/card";
+import './App.css';
+import React, { Component } from 'react'
+	
+class App extends Component {
+  constructor(props){
+	super(props)
+	this.state = {users_data :[],
+                loading : true                
+  }
+	this.displayUsers = this.displayUsers.bind(this)
+  }
+    
+  displayUsers(){
+      document.getElementById("main").style.display='flex';
+      const link="https://reqres.in/api/users?page=2";
+      fetch(link)
+      .then(response => response.json())
+      .then((users) => {
+        
+        this.setState({users_data :users.data,
+                        loading: false
+        })
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+    
+  render(){
+    return (<>
+        
+      <nav className="navbar navbar-expand-lg navbar-light rounded m-3">
+          <div className="container-fluid">
+            
+            <button >LetsGrowMore</button>
+            <button >HOME</button>
+            
+            <button >ABOUT</button>
+            <button onClick={this.displayUsers}>Get Users</button>
+          </div>
+        </nav>
+      <Users loading={this.state.loading} users={this.state.users_data}/>
+    </>
+    )
+  }
+}
+	
+export default App;
